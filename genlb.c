64a1bf77036f6f2347043a9476ad28bcfdd948a8
Michael Santos <michael.santos@gmail.com>
2019-02-27 10:48:08 -0500

    stderr: always log fatal messages

--- genlb.c
+++ genlb.c
@@ -177,22 +177,22 @@ static int genlb_tracee(genlb_state_t *s, char *argv[]) {
   (void)unsetenv("LD_PRELOAD");
 
   if (ptrace(PTRACE_TRACEME, 0, 0, 0) < 0) {
-    VERBOSE(s, 1, "ptrace(PTRACEME): %s\n", strerror(errno));
+    VERBOSE(s, 0, "ptrace(PTRACEME): %s\n", strerror(errno));
     return -1;
   }
 
   if (prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0) < 0) {
-    VERBOSE(s, 1, "prctl(PR_SET_NO_NEW_PRIVS): %s\n", strerror(errno));
+    VERBOSE(s, 0, "prctl(PR_SET_NO_NEW_PRIVS): %s\n", strerror(errno));
     return -1;
   }
 
   if (prctl(PR_SET_SECCOMP, SECCOMP_MODE_FILTER, &prog) < 0) {
-    VERBOSE(s, 1, "prctl(PR_SET_SECCOMP): %s\n", strerror(errno));
+    VERBOSE(s, 0, "prctl(PR_SET_SECCOMP): %s\n", strerror(errno));
     return -1;
   }
 
   if (raise(SIGSTOP) < 0) {
-    VERBOSE(s, 1, "raise(SIGSTOP): %s\n", strerror(errno));
+    VERBOSE(s, 0, "raise(SIGSTOP): %s\n", strerror(errno));
     return -1;
   }
 
@@ -203,18 +203,22 @@ static int genlb_tracer(genlb_state_t *s, pid_t tracee) {
   int status;
 
   if (waitpid(tracee, &status, 0) < 0) {
-    VERBOSE(s, 1, "waitpid: %s\n", strerror(errno));
+    VERBOSE(s, 0, "waitpid: %s\n", strerror(errno));
     return -1;
   }
 
   if (ptrace(PTRACE_SETOPTIONS, tracee, 0,
              PTRACE_O_TRACESECCOMP | PTRACE_O_EXITKILL | PTRACE_O_TRACEVFORK |
                  PTRACE_O_TRACEFORK | PTRACE_O_TRACECLONE |
-                 PTRACE_O_TRACEEXEC) < 0)
+                 PTRACE_O_TRACEEXEC) < 0) {
+    VERBOSE(s, 0, "ptrace(SETOPTIONS): %s\n", strerror(errno));
     return -1;
+  }
 
-  if (ptrace(PTRACE_CONT, tracee, 0, 0) < 0)
+  if (ptrace(PTRACE_CONT, tracee, 0, 0) < 0) {
+    VERBOSE(s, 0, "ptrace(CONT): %s\n", strerror(errno));
     return -1;
+  }
 
   return event_loop(s);
 }
